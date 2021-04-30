9ecb506410ec374a0a5a9c26619682ec53a2f979
Mario Koco <mario.koco92@gmail.com>
2021-04-30 09:54:59 +0200

    Stampate a schermo elenco film come homepage, sistemato css base

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ var app = new Vue({
     el: "#root",
 
     data: {
+        // Popular Movie set in homepage
+        popularMovie: [],
+    },
+    
+    methods: {
 
     },
 
-    methods: {
-
+    mounted() {
+        axios
+        .get('https://api.themoviedb.org/3/movie/popular?api_key=e205f242d3393f38b4790265be507509&language=it-IT&page=1')
+        .then((response) => {
+            this.popularMovie = response.data.results;
+            console.log(this.popularMovie)
+        });
     }
 })
