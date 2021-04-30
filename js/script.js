018010fecdca696f5c66f5de50a1f4c210c5a374
Mario Koco <mario.koco92@gmail.com>
2021-04-30 10:53:17 +0200

    aggiunta funzione che al click del button avvia la ricerca dei film

--- js/script.js
+++ js/script.js
@@ -5,12 +5,27 @@ var app = new Vue({
     data: {
         // Popular Movie set in homepage
         popularMovie: [],
+
+        // User input
+        search:'',
+
+        // Search Filter Movie
+        filteredMovie: [],
     },
     
     methods: {
-
+        startSearch()   {
+            // This get the movie searched.
+            axios
+            .get('https://api.themoviedb.org/3/search/movie/?api_key=e205f242d3393f38b4790265be507509&language=it-IT',{params : {query : this.search }})
+            .then((response) => {
+                this.filteredMovie = response.data.results;
+                console.log(this.filteredMovie)
+            });
+        }
     },
 
+    // This get the popular movie
     mounted() {
         axios
         .get('https://api.themoviedb.org/3/movie/popular?api_key=e205f242d3393f38b4790265be507509&language=it-IT&page=1')
@@ -18,5 +33,7 @@ var app = new Vue({
             this.popularMovie = response.data.results;
             console.log(this.popularMovie)
         });
-    }
+    },
+
+    
 })
