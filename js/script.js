2d8a7f9124109074f3828a6a36cd97ca52bcfc32
Mario Koco <mario.koco92@gmail.com>
2021-04-30 13:05:28 +0200

    sistemati errori relativi al tasto cerca e alla visualizzazione dei film su pagina, sistemati commenti e aggiuinta qualche funzione extra per comodità

--- js/script.js
+++ js/script.js
@@ -14,15 +14,24 @@ var app = new Vue({
     },
     
     methods: {
+        // this function will start when the button is clicked, and will insert in an array
+        // the list of objects containing the film searched by the user.
         startSearch()   {
             // This get the movie searched.
             axios
             .get('https://api.themoviedb.org/3/search/movie/?api_key=e205f242d3393f38b4790265be507509&language=it-IT',{params : {query : this.search }})
             .then((response) => {
                 this.filteredMovie = response.data.results;
-                console.log(this.filteredMovie)
+                this.search = '';  
             });
+        },
+
+        // this function cleans the array created by the search and re-displays popular movies.
+        // it's a kind of homepage call
+        home() {
+            return this.filteredMovie = [];
         }
+
     },
 
     // This get the popular movie
